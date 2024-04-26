feb82db0b57b13ed26665b7a351108ece67d9c01
fionaisaa <fionaisa04@gmail.com>
2024-04-26 22:51:55 +0200

    Ristrukturimi i follderave + new component
    Krijimi I komponentes Navigationbar, dhe ristrukturimi i follderave

--- frontend/internnexus/src/App.js
+++ frontend/internnexus/src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import './App.css';
-import internnexus from './imgsrc/internnexus.png';
-import { Nav, Container, Navbar } from 'react-bootstrap';
-
+import NavigationBar from './components/Navigationbar';
 
 function App() {
   return (
-    <body>
-     <Navbar className='NavbarContainer'>
-      <Container className='LogoContainer'>
-        <img src={internnexus} alt="Logo" className='logo' />
-      </Container>
-      <Nav className="NavigationContainer">
-        <Nav.Link href="#home">HOME</Nav.Link>
-        <Nav.Link href="#features">INTERNSHIPS</Nav.Link>
-        <Nav.Link href="#pricing">STUDENTS</Nav.Link>
-        <button className='buttonLogin'>LOG IN</button>
-      </Nav>
-    </Navbar>
-    </body>
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <body>
+      <NavigationBar></NavigationBar>
+      </body>
+    </div>
   );
 }
 
---
+++ frontend/internnexus/src/components/NavigationBar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import './App.css';
+import internnexus from './imgsrc/internnexus.png';
+
+
+function NavigationBar() {
+  return (
+    <body>
+     <Navbar className='NavbarContainer'>
+      <Container className='LogoContainer'>
+        <img src={internnexus} alt="Logo" className='logo' />
+      </Container>
+      <Nav className="NavigationContainer">
+        <Nav.Link href="#home">HOME</Nav.Link>
+        <Nav.Link href="#features">INTERNSHIPS</Nav.Link>
+        <Nav.Link href="#pricing">STUDENTS</Nav.Link>
+        <button className='buttonLogin'>LOG IN</button>
+      </Nav>
+    </Navbar>
+    </body>
+  );
+}
+
+export default NavigationBar;
