11209666e08cf17b4bd6a1d297d062c6b6de6df3
altinsyla <altinsyla11@gmail.com>
2024-04-29 19:54:36 +0200

    Internship connect with InternshipCards

--- frontend/internnexus/src/components/Internships/Internships.js
+++ frontend/internnexus/src/components/Internships/Internships.js
@@ -1,18 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../components/Internships/Internships.scss";
-import "../../components/Internships/InternshipCard.js";
 import InternshipCard from "../../components/Internships/InternshipCard.js";
 import NavigationBar from "../NavigationBar.js";
+import frakton from "../Internships/img/frakton.png";
+import xponent from "../Internships/img/xponent.jpg";
+import starlabs from "../Internships/img/starlabs.png";
+import ict from "../Internships/img/ict.png";
+import solaborate from "../Internships/img/solaborate.png";
 
 function Internships() {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  //Objekt me i marr t'dhanat
+  const internshipCards = [
+    {
+      title: "Front-End Developer",
+      type: "Full-time",
+      location: "Prishtina",
+      image: xponent,
+    },
+    {
+      title: "Back-End Developer",
+      type: "Part-time",
+      location: "Prishtina",
+      image: frakton,
+    },
+    {
+      title: "UI/UX Designer",
+      type: "Full-time",
+      location: "Prishtina",
+      image: starlabs,
+    },
+    {
+      title: "Game Developer",
+      type: "Part-time",
+      location: "Prizren",
+      image: ict,
+    },
+    {
+      title: "Front-End Developer",
+      type: "Full-time",
+      location: "Remote",
+      image: solaborate,
+    },
+  ];
+
+  // Na jep results edhe nese e dhajm me tvogla
+  const filteredInternshipCards = internshipCards.filter((card) =>
+    card.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
     <div>
       <NavigationBar />
       <body className="internship_body">
-        <InternshipCard />
-        <InternshipCard />
-        <InternshipCard />
-        <InternshipCard />
+        <input
+          type="text"
+          placeholder="Search..."
+          value={searchQuery}
+          onChange={handleSearchChange}
+          className="searchInput"
+        />
+        {filteredInternshipCards.map((card, index) => (
+          <InternshipCard key={index} {...card} />
+        ))}
       </body>
     </div>
   );
