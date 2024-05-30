f86c605d96883f38de20b51329195507cb69ae16
flamurisa <100003081+flamurisa@users.noreply.github.com>
2024-05-30 18:25:10 +0200

    Login backend

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -1,4 +1,6 @@
 const User = require("../Models/userModel");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const getAllUsers = async (req, res) => {
   const { username, fullname} = req.query;
@@ -35,6 +37,8 @@ const getcustomlimitusers = async (req, res) => {
   try {
     const Users = await User.find().limit(limit);
     res.status(200).json(Users);
+    const users = await User.find();
+    res.status(200).json(users);
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
@@ -50,6 +54,23 @@ const getSingleUser = async (req, res) => {
   }
 };
 
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (user) {
+      const isvalid = await user.isValidPassword(password);
+      if (isvalid) {
+        // username edhe pw correct
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET || 'qelsi', {
+          expiresIn: "1d",
+        });
+
+        res.json(token); //kthehet tokeni si response
+      }
+    } else {
+      res.status(404).json({ mesazhi: "Incorrect username or password" });
+    }};
+
 const createUser = async (req, res) => {
   const { username, fullname, email, password, role } = req.body;
   //   console.log(req.body);
@@ -59,17 +80,13 @@ const createUser = async (req, res) => {
   }
 
   try {
-    // Check for duplicate userID
     const existingUserUsername = await User.findOne({ username });
     const existingUserEmail = await User.findOne({ email });
     if (existingUserUsername) {
-      return res
-        .status(409)
-        .json({ message: "User with this username already exists!" });
-    } else if (existingUserEmail) {
-      return res
-        .status(409)
-        .json({ message: "User with this email already exists!" });
+      return res.status(409).json({ message: "User with this username already exists!" });
+    }
+    if (existingUserEmail) {
+      return res.status(409).json({ message: "User with this email already exists!" });
     }
 
     const newUser = await User.create({
@@ -89,7 +106,7 @@ const createUser = async (req, res) => {
 const updateUser = async (req, res) => {
   const id = req.params.id;
   try {
-    const updateUser = await User.findOneAndUpdate({ _id: id }, req.body, {
+    const updateUser = await User.findByIdAndUpdate(id, req.body, {
       new: true,
     });
     res.status(200).json(updateUser);
@@ -101,7 +118,7 @@ const updateUser = async (req, res) => {
 const deleteUser = async (req, res) => {
   const id = req.params.id;
   try {
-    await User.findByIdAndDelete({ _id: id });
+    await User.findByIdAndDelete(id);
     res.status(204).json({ message: "User deleted successfully!" });
   } catch (error) {
     res.status(400).json({ message: error.message });
@@ -115,6 +132,7 @@ module.exports = {
   updateUser,
   deleteUser,
   getcustomlimitusers,
+  loginUser,
 };
 
 
--- frontend/internnexus/src/components/Login.js
+++ frontend/internnexus/src/components/Login.js
@@ -1,59 +1,85 @@
-import React from 'react';
-
-// import { Button } from 'react-bootstrap';
-// qikjo metod osht ma e keqe per me marr componente mrena bootstrapit, metod ma e mir osht 
-// import Button from 'react-bootstrap/Button'; 
-// kjo e thirr veq Buttonin nkrahasim me ata ma nalt e cila e thirr KREJT librarin, ndikon ne performance edhe ne madhesi
-
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
 import { SiFacebook } from "react-icons/si";
-
 import { FcGoogle } from "react-icons/fc";
 import { useHistory } from 'react-router-dom';
 import internnexus from '../imgsrc/internnexus.png';
 import '../styles/App.css';
+import api from "../api";
+
+
+const Login = () => {
+  
+  useEffect(() => {
+    const checkLogin = async () => {
+      const token = localStorage.getItem("token");
+      if (token) {
+        history.push("/");
+      }
+    };
+    checkLogin();
+  }, []);
 
 
 
+const history = useHistory();
 
-const fontiPerTekste = 'robot-font';
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [token, setToken] = useState("");
 
-function Login() {
+  const handleEmailChange = (event) => setEmail(event.target.value);
+  const handlePasswordChange = (event) => setPassword(event.target.value);
 
-  const history = useHistory();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log("Logged in Successfully");
+    console.log(email, password);
+    
+    try {
+      const response = await api.post("/user/login", { email, password });
+      localStorage.setItem("token", response.data);
+      setToken(response.data);
+      history.push("/");
+    } catch (error) {
+      console.log("Login Error");
+    }
+  };
 
   const handleSignup = () => {
     history.push('/signupform');
-  }
+  };
+
+  const fontiPerTekste = 'robot-font';
 
   return (
     <div>
-        <div className='mainDivLogin'>
-          <img src={internnexus} alt='Logo' className='logo'/>
-          <p style={{fontWeight: 'bold',fontFamily:{fontiPerTekste},fontSize: 30 }}>Log in to InternNexus</p>
-          <form>
-            <input form='email' placeholder='Phone Number, email address' className='Inputs'/>
-            <input type='password' placeholder='Password' className='Inputs'/>
-          </form>
-          <button className='loginBtn'>Log In</button>
-          <div className='forgotPasswordDiv'>
+      <div className='mainDivLogin'>
+        <img src={internnexus} alt='Logo' className='logo' />
+        <p style={{ fontWeight: 'bold', fontFamily: fontiPerTekste, fontSize: 30 }}>Log in to InternNexus</p>
+        <form onSubmit={handleSubmit}>
+          <input type='email' placeholder='Email address' className='Inputs' value={email} onChange={handleEmailChange} />
+          <input type='password' placeholder='Password' className='Inputs' value={password} onChange={handlePasswordChange} />
+          <button type='submit' className='loginBtn'>Log In</button>
+        </form>
+        <div className='forgotPasswordDiv'>
           <a href='#' className='signuptext'>Forgot password?</a>
           <a href='#' className='signuptext' onClick={handleSignup}>Sign Up to InternNexus</a>
-          </div>
-
-      <div className='btn-google-fb'>
-
-        <button className='googlebtn'>
-      <FcGoogle className='googleicon'/>
-          Login with Google</button>
-
-          <buton className='facebookbtn'>
-          <SiFacebook className='fbicon' />
-
-            Login With Facebook</buton>
-      </div>
         </div>
+        <div className='btn-google-fb'>
+          <button className='googlebtn'>
+            <FcGoogle className='googleicon' />
+            Login with Google
+          </button>
+          <button className='facebookbtn'>
+            <SiFacebook className='fbicon' />
+            Login With Facebook
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default Login;
+                     
